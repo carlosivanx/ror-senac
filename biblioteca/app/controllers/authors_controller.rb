4332eb7a3b76ccd19dbddb395b0f1f50902abc6a
Carlos Ivan <carlosivanx@hotmail.com>
2017-07-29 17:09:09 -0300

    Adicionado a pesquisa ao author como foi feito no livro

--- biblioteca/app/controllers/authors_controller.rb
+++ biblioteca/app/controllers/authors_controller.rb
@@ -1,11 +1,21 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :edit, :update, :destroy]
 
+
+
+
   def index
-    @authors = Author.paginate(page: params[:page], per_page: 5)
+    #  @authors = Author.paginate(page: params[:page], per_page: 5)
+    if params[:search].present?
+       @authors = Author.where("name like '%#{params[:search]}%' ")
+    else
+       @authors = Author.all
+    end
   end
 
-  def show;end
+  def
+    show;
+  end
 
   def new
     @author = Author.new
